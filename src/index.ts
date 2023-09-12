05b90c116531c826c657413a75d9361a2356faac
Christine Lee <lee.christine@northeastern.edu>
2023-09-12 11:20:22 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
+const result_multiply = calc.multiply(2, 3);
+
 console.log(`result_add = ${result_add}`);
+console.log(`result_multiply = ${result_multiply}`); // 6
 
